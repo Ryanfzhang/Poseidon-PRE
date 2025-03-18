4b00b4f404c430e574c8304fdc05f8258732df89
Ryan Zhang <mafzhang@ust.hk>
2025-03-18 22:19:01 +0800

    version 3

--- model/oceanformer.py
+++ model/oceanformer.py
@@ -329,7 +329,7 @@ class Xuanming(nn.Module):
 if __name__=="__main__":
     model = Xuanming()
     model = model.to("cuda")
-    x = torch.randn(2, 19, 30, 400, 441).to("cuda")
+    x = torch.randn(1, 19, 30, 400, 441).to("cuda")
     x_mark = torch.Tensor([[11, 30], [1,28]]).to("cuda")
     y_mark = torch.Tensor([[11, 30], [1,28]]).to("cuda")
     y = model(x, x_mark, y_mark)
