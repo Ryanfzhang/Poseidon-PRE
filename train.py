1066ec0e7bbe5bd20a9ba44752ac1a99041f9241
Ryan Zhang <mafzhang@ust.hk>
2025-03-20 11:17:28 +0800

    batch size

--- train.py
+++ train.py
@@ -35,7 +35,7 @@ parser.add_argument('--drivers', type=int, default=19, help='input sequence leng
 
 # optimization
 parser.add_argument('--train_epochs', type=int, default=100, help='train epochs')
-parser.add_argument('--batch_size', type=int, default=1, help='batch size of train input data')
+parser.add_argument('--batch_size', type=int, default=3, help='batch size of train input data')
 parser.add_argument('--learning_rate', type=float, default=5e-4, help='optimizer learning rate')
 parser.add_argument('--weight_decay', type=float, default=3e-6, help='optimizer wd')
 parser.add_argument('--loss', type=str, default='mae', help='loss function')
