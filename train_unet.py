32eeaa9fdd7761da13e48a679be8e01cc7d179bd
Ryanfzhang <mafzhang@ust.hk>
2025-03-28 22:26:41 +0800

    unet

--- train_unet.py
+++ train_unet.py
@@ -41,7 +41,7 @@ parser.add_argument('--beta', type=float, default=0.2, help='input sequence leng
 
 # optimization
 parser.add_argument('--train_epochs', type=int, default=100, help='train epochs')
-parser.add_argument('--batch_size', type=int, default=2, help='batch size of train input data')
+parser.add_argument('--batch_size', type=int, default=6, help='batch size of train input data')
 parser.add_argument('--learning_rate', type=float, default=1e-3, help='optimizer learning rate')
 parser.add_argument('--weight_decay', type=float, default=1e-5, help='optimizer wd')
 parser.add_argument('--loss', type=str, default='mae', help='loss function')
@@ -51,9 +51,9 @@ args = parser.parse_args()
 check_dir(args.checkpoints)
 
 train_dataset = NetCDFDataset(dataset_path=args.dataset_path, lead_time=args.lead_time)
-train_dloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=8, prefetch_factor=4)
+train_dloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=4, prefetch_factor=1)
 test_dataset = NetCDFDataset(startDate='20200101', endDate='20221228', dataset_path=args.dataset_path, lead_time=args.lead_time)
-test_dloader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, drop_last=True, num_workers=8, prefetch_factor=4)
+test_dloader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, drop_last=True, num_workers=4, prefetch_factor=1)
 
 model = UNet()
 optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay, betas=(0.9, 0.995))
